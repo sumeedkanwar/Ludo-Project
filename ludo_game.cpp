5d0a64921267dfae0c6df69d8c68c2a887453a91
Abdul Rafay <abdulrafay5145593@gmial.com>
2024-11-30 21:06:07 +0500

    Refactor killer paths for improved readability and consistency

--- ludo_game.cpp
+++ ludo_game.cpp
@@ -136,62 +136,7 @@ void LudoGame::initializeGame()
 
     
     std::vector<sf::Vector2i> killerRedLudoPath = {// part of ludo path starting from red spawn position to 7,0 then in to home column
-                         {6, 1},
-                         {6, 2},
-                         {6, 3},
-                         {6, 4},
-                         {6, 5},
-                         {5, 6},
-                         {4, 6},
-                         {3, 6},
-                         {2, 6},
-                         {1, 6},
-                         {0, 6},
-                         {0, 7},
-                         {0, 8},
-                         {1, 8},
-                         {2, 8},
-                         {3, 8},
-                         {4, 8},
-                         {5, 8},
-                         {6, 9},
-                         {6, 10},
-                         {6, 11},
-                         {6, 12},
-                         {6, 13},
-                         {6, 14},
-                         {7, 14},
-                         {8, 14},
-                         {8, 13},
-                         {8, 12},
-                         {8, 11},
-                         {8, 10},
-                         {8, 9},
-                         {9, 8},
-                         {10, 8},
-                         {11, 8},
-                         {12, 8},
-                         {13, 8},
-                         {14, 8},
-                         {14, 7},
-                         {14, 6},
-                         {13, 6},
-                         {12, 6},
-                         {11, 6},
-                         {10, 6},
-                         {9, 6},
-                         {8, 5},
-                         {8, 4},
-                         {8, 3},
-                         {8, 2},
-                         {8, 1},
-                         {8, 0},
-                         {7, 0},
-                         {7, 1},
-                         {7, 2},
-                         {7, 3},
-                         {7, 4},
-                         {7, 5}};
+    {6, 1},{6, 2},{6, 3},{6, 4},{6, 5},{5, 6},{4, 6},{3, 6},{2, 6},{1, 6},{0, 6},{0, 7},{0, 8},{1, 8},{2, 8},{3, 8},{4, 8},{5, 8},{6, 9},{6, 10},{6, 11},{6, 12},{6, 13},{6, 14},{7, 14},{8, 14},{8, 13},{8, 12},{8, 11},{8, 10},{8, 9},{9, 8},{10, 8},{11, 8},{12, 8},{13, 8},{14, 8},{14, 7},{14, 6},{13, 6},{12, 6},{11, 6},{10, 6},{9, 6},{8, 5},{8, 4},{8, 3},{8, 2},{8, 1},{8, 0},{7, 0},{7, 1},{7, 2},{7, 3},{7, 4},{7, 5}};
 
     std::vector<sf::Vector2i> killerGreenLudoPath = {// part of ludo path starting from green spawn position to 0,7 then in to home column of green
                            {1, 8},
@@ -421,6 +366,8 @@ int LudoGame::rollDice()
 
 sf::Vector2i LudoGame::moveTokenOnBoard(sf::Vector2i token, int player)
 {
+    const std::vector<sf::Vector2i>& path = killers[player] ? killersPath[player] : ludoPath;
+
     auto it = std::find(ludoPath.begin(), ludoPath.end(), token);
     if (it == ludoPath.end())
         return token;
@@ -466,6 +413,8 @@ void LudoGame::moveToken(int player, int tokenIndex)
     {
         if (otherPlayer != player)
         {
+            killers[player] = true;
+
             for (auto &otherToken : playerTokens[otherPlayer])
             {
                 if (otherToken == token && !isSafeZone(token))
